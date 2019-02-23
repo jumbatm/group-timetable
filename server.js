a86ad059b662cb78e1ad54808d7dba2716082108
swordofjustice <damoproductionsandroid@gmail.com>
2019-02-23 16:58:02 +1000

    back to original

--- server.js
+++ server.js
@@ -1,25 +1,8 @@
 "use strict";
 
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 let app = express();
 const port = 3000;
-
-// connect to the db and start the express server
-let db;
-
-const url =  'mongodb://localhost:21017/clicks';
-
-MongoClient.connect(url, (err, database) => {
-  if(err) {
-    return console.log(err);
-  }
-  db = database;
-  // start the express web server listening on 3000
-  app.listen(3000, () => {
-    console.log('listening on 3000');
-  });
-});
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
@@ -28,23 +11,3 @@ app.get('/:sessionId', function (req, res) {
 });
 
 app.listen(port, () => console.log("Example app listening on port ${port} !"));
-
-// serve the homepage
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
-
-// add a document to the DB collection recording the click event
-app.post('/clicked', (req, res) => {
-  const click = {clickTime: new Date()};
-  console.log(click);
-  console.log(db);
-
-  db.collection('clicks').save(click, (err, result) => {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('click added to db');
-    res.sendStatus(201);
-  });
-});
